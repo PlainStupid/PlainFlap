61086a8f06dcf229ae911999f630993fb1271a0b
kristinnv12 <kristinnv12@ru.is>
2014-04-03 14:42:33 +0000

    em

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -67,9 +67,6 @@ window.Player = (function() {
 
     Player.prototype.checkCollisionWithBounds = function() {
         if (this.pos.y + HEIGHT > this.game.WORLD_HEIGHT) {
-            this.el.css('transform', 'rotate(30deg)');
-            this.el.css('-ms-transform','rotate(30deg)');
-            this.el.css('-webkit-transform','rotate(30deg)');
             return this.game.gameover();
         }
         if (this.pos.y <= 0) {
